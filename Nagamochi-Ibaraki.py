03bc121a51343e6857f008cffcb5244a8b9a2df5
Akshay <akxrawat.ar@gmail.com>
2017-10-11 22:45:34 -0500

    2nd commit

--- Nagamochi-Ibaraki.py
+++ Nagamochi-Ibaraki.py
@@ -1,17 +1,30 @@
-import random
+import random as rd
 import networkx as nx
+import matplotlib as mt
+import matplotlib.pyplot as plt
+import tkinter
 
-def createGraph(n,m):
+#def nagamochiIbaraki():
+
+def createGraph(n,edge):
     global adjList
-    for i in range(n):
+    G = nx.Graph()
+    g = G.gnm_random_graph(n,edge)
+    nx.draw(g)
+    plt.show()
+    '''for i in range(n):
         for j in range(n):
             if i == j:
                 adjList[i][j] = adjList[j][i] = 0
             else:
-
+                x = rd.randint(0,1)
+                if(x == 1):
+                    adjList[i][j] = adjList[j][i] = adjList[i][j] + 1
+                    
+    '''
 
 
 n = 19      # nodes are set to 19
 for m in range(20, 168, 4):  # edges varies from 20 to 168 in steps of 4
     createGraph(n,m)
-    nagamochiIbaraki(X,n)
+    #nagamochiIbaraki(X,n)
