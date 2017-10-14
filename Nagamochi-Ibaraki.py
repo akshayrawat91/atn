3da36ddddb58efc24a9101df27d18936bb5fc47b
akshay <31359221+akshayrawat91@users.noreply.github.com>
2017-10-13 21:03:19 -0500

    Update Nagamochi-Ibaraki.py

--- Nagamochi-Ibaraki.py
+++ Nagamochi-Ibaraki.py
@@ -1,6 +1,7 @@
 # @author akshayrawat91
 # finds the connectivity for a pseudo-random connected graph
-# reference - https://github.com/GUHAN1990/Nagamochi-Ibaraki-Algorithm-Implementation/blob/master/algo_implementation.py
+# reference - GUHAN1990 "Nagamochi-Ibaraki-Algorithm-Implementation"
+# https://github.com/GUHAN1990/Nagamochi-Ibaraki-Algorithm-Implementation/blob/master/algo_implementation.py
 
 import random as rd
 from networkx import *
